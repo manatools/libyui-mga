5fd4cf6bd9e704072ed80218890596a47dde0c12
Angelo Naselli <anaselli@linux.it>
2013-10-28 21:49:43 +0100

    Added EXTENSIONNAME instead of PLUGINNAME

--- PROJECTINFO.cmake
+++ PROJECTINFO.cmake
@@ -5,7 +5,7 @@ SET( BASELIB		"yui" )		# don't change this
 ##### MAKE ALL NEEDED CHANGES HERE #####
 
 SET( SUBDIRS		src examples)	        # set the subdirs where your code is located.
-SET( PLUGINNAME		"mga" )		# define the your plugin's name, e.g. {gtk, ncurses-pkg, qt-graph}
+SET( EXTENSIONNAME	"mga" )		# define the your plugin's name, e.g. {gtk, ncurses-pkg, qt-graph}
 #SET( LIB_DEPS		Qt)		# define the plugin's dependecies CMake should look for separated with spaces; libyui-internal deps go in the next line
 SET( INTERNAL_DEPS	Libyui )	# include libyui or it's plugins (if needed), too!
 #SET( LIB_LINKER		QtCore QtGui X11 fontconfig dl pthread )	# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!

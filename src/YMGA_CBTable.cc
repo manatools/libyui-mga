c81d57cf947ba00a8bdc5cf83f7b643195ebaa8a
Angelo Naselli <anaselli@linux.it>
2013-11-13 22:26:22 +0100

    Avoid YTable and use YSelectionWidget directly, so that only yui-mga plugin GUI implementation is called. TODO check and remove useles code

--- src/YMGA_CBTable.cc
+++ src/YMGA_CBTable.cc
@@ -33,16 +33,16 @@
 struct YMGA_CBTablePrivate
 {
     YMGA_CBTablePrivate( YTableHeader * header )
-	:/* header( header )
+	: header( header )
 	, keepSorting( false )
 	, immediateMode( false )
-        ,*/ mode( YTableSingleLineSelection )
+        , mode( YTableSingleLineSelection )
     {
     }
 
-//     YTableHeader *	header;
-//     bool		keepSorting;
-//     bool		immediateMode;
+     YTableHeader *	header;
+     bool		keepSorting;
+     bool		immediateMode;
     YTableMode          mode;
 };
 
@@ -50,7 +50,9 @@ struct YMGA_CBTablePrivate
 
 
 YMGA_CBTable::YMGA_CBTable( YWidget* parent, YTableHeader * header, YTableMode mode )
-    : YTable( parent, header, mode==YTableSingleLineSelection) // enforceSingleSelection
+    :YSelectionWidget( parent,
+                        "",     // label
+                        mode==YTableSingleLineSelection ) // enforceSingleSelection
     , priv( new YMGA_CBTablePrivate( header ) )
 {
     YUI_CHECK_PTR( header );
@@ -71,5 +73,170 @@ YMGA_CBTable::~YMGA_CBTable()
 YTableMode YMGA_CBTable::selectionMode()
 {
   return priv->mode;
+}
+
+
+// void YMGA_CBTable::addItem ( YItem* item )
+// {
+//   YUI_CHECK_PTR( item );
+//   bool selected = item->selected();
+//     
+//   YSelectionWidget::addItem ( item );
+//   
+//   // get rid of YSelectionWidget::addItem that selects first item
+//   item->setSelected(selected);
+// }
+
+void
+YMGA_CBTable::setTableHeader( YTableHeader * newHeader )
+{
+    YUI_CHECK_PTR( newHeader );
+
+    if ( priv->header->columns() != newHeader->columns() )
+        deleteAllItems();
+
+    delete priv->header;
+    priv->header = newHeader;
+}
+
+
+int
+YMGA_CBTable::columns() const
+{
+    return priv->header->columns();
+}
+
+
+bool
+YMGA_CBTable::hasColumn( int column ) const
+{
+    return priv->header->hasColumn( column );
+}
+
+
+std::string
+YMGA_CBTable::header( int column ) const
+{
+    return priv->header->header( column );
+}
+
+
+YAlignmentType
+YMGA_CBTable::alignment( int column ) const
+{
+    return priv->header->alignment( column );
+}
+
+
+bool
+YMGA_CBTable::immediateMode() const
+{
+    return priv->immediateMode;
+}
+
+
+void
+YMGA_CBTable::setImmediateMode( bool immediateMode )
+{
+    priv->immediateMode = immediateMode;
+
+    if ( immediateMode )
+        setNotify( true );
+}
+
+
+bool
+YMGA_CBTable::keepSorting() const
+{
+    return priv->keepSorting;
+}
+
+
+void
+YMGA_CBTable::setKeepSorting( bool keepSorting )
+{
+    priv->keepSorting = keepSorting;
+}
+
+
+bool
+YMGA_CBTable::hasMultiSelection() const
+{
+    return ! YSelectionWidget::enforceSingleSelection();
+}
+
+
+const YPropertySet &
+YMGA_CBTable::propertySet()
+{
+    static YPropertySet propSet;
+
+    if ( propSet.isEmpty() )
+    {
+        /*
+         * @property itemID             Value           The currently selected item
+         * @property itemID             CurrentItem     The currently selected item
+         * @property itemList           Items           All items
+         * @property itemList           SelectedItems   All currently selected items
+         * @property std::string        Cell            One cell (one column of one item)
+         * @property integer            Cell            (ChangeWidget only) One cell as integer
+         * @property `icon(...)         Cell            Icon for one one cell
+         * @property std::string        Item            Alias for Cell
+         * @property std::string        Item            QueryWidget only: Return one complete item
+         * @property std::string        IconPath        Base path for icons
+         * @property bool               MultiSelection  Flag: User can select multiple items (read-only)
+         */
+        propSet.add( YProperty( YUIProperty_Value,              YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_CurrentItem,        YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_SelectedItems,      YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_Items,              YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_Cell,               YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_Item,               YOtherProperty   ) );
+        propSet.add( YProperty( YUIProperty_IconPath,           YStringProperty  ) );
+        propSet.add( YProperty( YUIProperty_MultiSelection,     YBoolProperty,   true ) ); // read-only
+        propSet.add( YWidget::propertySet() );
+    }
+
+    return propSet;
+}
+
+
+bool
+YMGA_CBTable::setProperty( const std::string & propertyName, const YPropertyValue & val )
+{
+    propertySet().check( propertyName, val.type() ); // throws exceptions if not found or type mismatch
+
+    if      ( propertyName == YUIProperty_Value         )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_CurrentItem   )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_SelectedItems )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_Items         )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_Cell          )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_Item          )       return false; // Needs special handling
+    else if ( propertyName == YUIProperty_IconPath      )       setIconBasePath( val.stringVal() );
+    else
+    {
+        return YWidget::setProperty( propertyName, val );
+    }
+
+    return true; // success -- no special processing necessary
+}
+
+
+YPropertyValue
+YMGA_CBTable::getProperty( const std::string & propertyName )
+{
+    propertySet().check( propertyName ); // throws exceptions if not found
+
+    if      ( propertyName == YUIProperty_Value         )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_CurrentItem   )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_SelectedItems )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_Items         )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_Cell          )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_Item          )       return YPropertyValue( YOtherProperty );
+    else if ( propertyName == YUIProperty_IconPath      )       return YPropertyValue( iconBasePath() );
+    else
+    {
+        return YWidget::getProperty( propertyName );
+    }
 }
 
--- src/YMGA_CBTable.h
+++ src/YMGA_CBTable.h
@@ -44,7 +44,7 @@ enum YTableMode {
 class YMGA_CBTablePrivate;
 
 /**
- * See document of YTable
+ * See document of YMGA_CBTable
  * Table: Selection list with multiple columns. The user can select exactly one
  * row (with all its columns) from that list. Each cell (each column within
  * each row) has a label text and an optional icon (*).
@@ -62,7 +62,7 @@ class YMGA_CBTablePrivate;
  * so an icon should never be an exclusive means to display any kind of
  * information.
  **/
-class YMGA_CBTable: public YTable 
+class YMGA_CBTable: public YSelectionWidget
 {
 protected:
     /**
@@ -92,6 +92,67 @@ public:
      **/
     virtual const char * widgetClass() const { return "YMGA_CBTable"; }
 
+    /**
+     * Return the number of columns of this table.
+     **/
+    int columns() const;
+
+    /**
+     * Return 'true' if this table has a column no. 'column'
+     * (counting from 0 on).
+     **/
+    bool hasColumn( int column ) const;
+
+    /**
+     * Return the header text for the specified column.
+     **/
+    std::string header( int column ) const;
+
+    /**
+     * Return the alignment for the specified column.
+     **/
+    YAlignmentType alignment( int column ) const;
+
+    /**
+     * Deliver even more events than with notify() set.
+     *
+     * With "notify" alone, a table widget sends an ActivatedEvent when the
+     * user double-clicks an item or presses the "space" key on it. It does
+     * not send an event when the user just sends another item.
+     *
+     * With "immediate", it also sends a SelectionChangedEvent when the user
+     * selects another item. "immediate" implicitly includes "notify".
+     **/
+    bool immediateMode() const;
+
+    /**
+     * Set immediateMode() on or off.
+     **/
+    void setImmediateMode( bool immediateMode = true );
+
+    /**
+     * Return 'true' if the sort order is to be kept in item insertion order,
+     * i.e. if sorting the table by clicking on a column header should be
+     * disabled.
+     **/
+    bool keepSorting() const;
+
+    /**
+     * Switch between sorting by item insertion order (keepSorting: true) or
+     * allowing the user to sort by an arbitrary column (by clicking on the
+     * column header).
+     *
+     * Derived classes can overwrite this function, but they should call this
+     * base class function in the new implementation.
+     **/
+    virtual void setKeepSorting( bool keepSorting );
+
+    /**
+     * Return 'true' if the user can select multiple items  at the same time
+     * (e.g., with shift-click or ctrl-click).
+     **/
+    bool hasMultiSelection() const;
+
      /**
      * returns the YTable selection mode
      *
@@ -112,7 +173,63 @@ public:
      **/
     virtual void cellChanged( const YTableCell * cell ) = 0;
 
-   
+    /**
+     * Set a property.
+     * Reimplemented from YWidget.
+     *
+     * This function may throw YUIPropertyExceptions.
+     *
+     * This function returns 'true' if the value was successfully set and
+     * 'false' if that value requires special handling (not in error cases:
+     * those are covered by exceptions).
+     **/
+    virtual bool setProperty( const std::string & propertyName,
+			      const YPropertyValue & val );
+
+    /**
+     * Get a property.
+     * Reimplemented from YWidget.
+     *
+     * This method may throw YUIPropertyExceptions.
+     **/
+    virtual YPropertyValue getProperty( const std::string & propertyName );
+
+    /**
+     * Return this class's property set.
+     * This also initializes the property upon the first call.
+     *
+     * Reimplemented from YWidget.
+     **/
+    virtual const YPropertySet & propertySet();
+
+
+    /**
+     * The name of the widget property that will return user input.
+     * Inherited from YWidget.
+     **/
+    const char * userInputProperty() { return YUIProperty_CurrentItem; }
+
+     /**
+     * Add one item. This widget assumes ownership of the item object and will
+     * delete it in its destructor.
+     *
+     * NOTE: For tree items, call this only for the toplevel items; all
+     * non-toplevel items are already owned by their respective parent
+     * items. Adding them to the parent widget will clash with this ownership.
+     *
+     * Reimplementation of YSelectionWidget::addItem.
+     **/
+//     virtual void addItem( YItem * item_disown );
+
+    /**
+     * Exchange the previous table header with a new one. This will delete the
+     * old YTableHeader object.
+     *
+     * If the new header has a different number of columns than the old one,
+     * all items will implicitly be deleted.
+     **/
+    void setTableHeader( YTableHeader * newHeader );
+
 private:
 
     ImplPtr<YMGA_CBTablePrivate> priv;

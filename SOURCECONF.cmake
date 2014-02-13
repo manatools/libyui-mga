7d33f641547a74dc0f9aabdcd7f4b01c39b39a48
_pmat_ <matteo@mageia.it>
2014-02-13 19:32:00 +0100

    compile with YMGAAboutDialog

--- SOURCECONF.cmake
+++ SOURCECONF.cmake
@@ -1,16 +1,19 @@
 SET( ${TARGETLIB}_SOURCES
     YMGA_CBTable.cc
+    YMGAAboutDialog.cc
     YMGAWidgetExtensionFactory.cc
 )
 
 SET( ${TARGETLIB}_HEADERS
     ##### Here go the headers
     YMGA_CBTable.h
+    YMGAAboutDialog.h
     YMGAWidgetExtensionFactory.h
 )
 
 SET( EXAMPLES_LIST
   ##### Here go the examples
+  AboutDialog.cc
   ManyWidgets.cc
 )
 

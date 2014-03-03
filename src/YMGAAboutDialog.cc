95cd814a68205a2e452312a572c552fa23a8e4db
_pmat_ <matteo@mageia.it>
2014-03-03 22:04:30 +0100

    - now provides two about boxes: Classic or Tabbed - moved createPopupDialog inside Classic and Tabbed methods - passing authors string as an argument to the constructor

--- src/YMGAAboutDialog.cc
+++ src/YMGAAboutDialog.cc
@@ -18,6 +18,7 @@
 #include "YUILog.h"
 
 #include <yui/YUI.h>
+#include <yui/YOptionalWidgetFactory.h>
 #include <yui/YApplication.h>
 #include <yui/YWidgetFactory.h>
 #include <yui/YDialog.h>
@@ -27,6 +28,8 @@
 #include <yui/YEvent.h>
 #include <yui/YImage.h>
 #include <yui/YRichText.h>
+#include <yui/YDumbTab.h>
+#include <yui/YReplacePoint.h>
 
 #include <boost/algorithm/string/trim.hpp>
 
@@ -42,14 +45,24 @@ public:
   std::string appName;
   std::string appVersion;
   std::string appLicense;
-  std::string appAuthor;
+  std::string appAuthors;
   std::string appDescription;
   std::string appIcon;
+  std::string appCredits;
+  std::string appInformation;
 
   YDialog* mainDialog;
 };
 
-YMGAAboutDialog::YMGAAboutDialog(const std::string& name, const std::string& version, const std::string& license, const std::string& description, const std::string& icon) :
+YMGAAboutDialog::YMGAAboutDialog(const std::string& name, 
+				 const std::string& version, 
+				 const std::string& license, 
+				 const std::string& authors,
+				 const std::string& description, 
+				 const std::string& icon,
+				 const std::string& credits,
+				 const std::string& information
+				) :
     priv ( new YMGAAboutDialogPrivate()) 
 {
   YUI_CHECK_NEW ( priv );
@@ -57,9 +70,12 @@ YMGAAboutDialog::YMGAAboutDialog(const std::string& name, const std::string& ver
   priv->appName = name;
   priv->appVersion = version;
   priv->appLicense = license;
+  priv->appAuthors = authors;
   priv->appDescription = description;
   priv->appIcon = icon;
-  priv->mainDialog = YUI::widgetFactory()->createPopupDialog();
+  priv->appCredits = credits;
+  priv->appInformation = information;
+  boost::algorithm::trim(priv->appIcon);
 }
 
 YMGAAboutDialog::~YMGAAboutDialog()
@@ -67,9 +83,141 @@ YMGAAboutDialog::~YMGAAboutDialog()
   delete priv;
 }
 
-void YMGAAboutDialog::start()
+void YMGAAboutDialog::showInformation()
 {
-  YUI::app()->setApplicationTitle(priv->appName);
+  auto infoDialog = YUI::widgetFactory()->createPopupDialog();
+  auto vbox = YUI::widgetFactory()->createVBox(infoDialog);
+  YUI::widgetFactory()->createLabel(vbox,"Information");
+  auto rtcontent = YUI::widgetFactory()->createRichText(vbox);
+  rtcontent->setText(priv->appInformation);
+  auto closeButton = YUI::widgetFactory()->createPushButton(vbox,"Close");
+  while(true)
+  {
+    YEvent* event = infoDialog->waitForEvent();
+    if(event)
+    {
+      // window manager "close window" button
+      if ( event->eventType() == YEvent::CancelEvent
+        || event->widget() == closeButton )
+        break; // leave event loop
+    }
+  }
+  infoDialog->destroy();
+}
+
+void YMGAAboutDialog::showCredits()
+{
+  auto creditDialog = YUI::widgetFactory()->createPopupDialog();
+  auto vbox = YUI::widgetFactory()->createVBox(creditDialog);
+  YUI::widgetFactory()->createLabel(vbox,"Credits");
+  auto rtcontent = YUI::widgetFactory()->createRichText(vbox);
+  rtcontent->setText(priv->appCredits);
+  auto closeButton = YUI::widgetFactory()->createPushButton(vbox,"Close");
+  while(true)
+  {
+    YEvent* event = creditDialog->waitForEvent();
+    if(event)
+    {
+      // window manager "close window" button
+      if ( event->eventType() == YEvent::CancelEvent
+        || event->widget() == closeButton )
+        break; // leave event loop
+    }
+  }
+  creditDialog->destroy();
+}
+
+void YMGAAboutDialog::genAuthorsTab(YReplacePoint* rpoint)
+{
+  rpoint->deleteChildren();
+  auto hbox = YUI::widgetFactory()->createHBox(rpoint);
+  YUI::widgetFactory()->createRichText(hbox,priv->appAuthors);
+  rpoint->showChild();
+  priv->mainDialog->recalcLayout();
+}
+
+void YMGAAboutDialog::genContributorsTab(YReplacePoint* rpoint)
+{
+  rpoint->deleteChildren();
+  auto hbox = YUI::widgetFactory()->createHBox(rpoint);
+  YUI::widgetFactory()->createRichText(hbox,priv->appDescription);
+  rpoint->showChild();
+  priv->mainDialog->recalcLayout();
+}
+
+void YMGAAboutDialog::Tabbed()
+{
+  priv->mainDialog = YUI::widgetFactory()->createPopupDialog();
+  YUI::app()->setApplicationTitle("About " + priv->appName);
+  auto vbox = YUI::widgetFactory()->createVBox(priv->mainDialog);
+  
+  /*
+   * Layout 
+   * _____________
+   * |             |
+   * |___CREDITS___|
+   * |_a_|_b_|_c___|
+   * |             |
+   * |             |
+   * |_____________|
+   */
+  
+  YUI::widgetFactory()->createSpacing(vbox,YD_VERT,false,1.0);
+  auto tophbox = YUI::widgetFactory()->createHBox(vbox);
+  YUI::widgetFactory()->createSpacing(tophbox,YD_HORIZ,false,2.0);
+  YUI::widgetFactory()->createLabel(tophbox,priv->appCredits);
+  YUI::widgetFactory()->createSpacing(tophbox,YD_HORIZ,false,2.0);
+  YUI::widgetFactory()->createSpacing(vbox,YD_VERT,false,1.0);
+  
+  if(YUI::optionalWidgetFactory()->hasDumbTab())
+  {
+    auto dumbTab = YUI::optionalWidgetFactory()->createDumbTab(vbox);
+    dumbTab->addItem(new YItem("Authors"));
+    dumbTab->addItem(new YItem("Description"));
+    
+    auto bottomvbox = YUI::widgetFactory()->createVBox(vbox);
+    auto rpoint = YUI::widgetFactory()->createReplacePoint(bottomvbox);
+    
+    this->genAuthorsTab(rpoint);
+    
+    auto cancelButton = YUI::widgetFactory()->createPushButton(vbox,"Close");
+    
+    while(true)
+    {
+      YEvent* event = priv->mainDialog->waitForEvent();
+      if(event)
+      {
+	// window manager "close window" button
+	if ( event->eventType() == YEvent::CancelEvent
+	  || event->widget() == cancelButton )
+	  break; // leave event loop
+	if ( event->eventType() == YEvent::MenuEvent )
+	{
+	  if( event->item()->label().replace(event->item()->label().find("&"),1,"").compare("Authors")==0 )
+	  {
+	    this->genAuthorsTab(rpoint);
+	  }
+	  else if( event->item()->label().replace(event->item()->label().find("&"),1,"").compare("Description")==0 )
+	  {
+	    this->genContributorsTab(rpoint);
+	  }
+	}
+      }
+    }
+    priv->mainDialog->destroy();
+  }
+  else
+  {
+    // handle it
+  }
+}
+
+void YMGAAboutDialog::Classic()
+{
+  YPushButton* creditsButton;
+  YPushButton* infoButton;
+  priv->mainDialog = YUI::widgetFactory()->createPopupDialog();
+  YUI::app()->setApplicationTitle("About " + priv->appName);
   auto vbox = YUI::widgetFactory()->createVBox(priv->mainDialog);
   auto tophbox = YUI::widgetFactory()->createHBox(vbox);
   if(priv->appIcon.compare("")!=0)
@@ -81,15 +229,22 @@ void YMGAAboutDialog::start()
   auto toprightvbox = YUI::widgetFactory()->createVBox(tophbox);
   toprightvbox->setWeight(YD_HORIZ, 5);
   auto lblAppName = YUI::widgetFactory()->createLabel(toprightvbox, "");
-  lblAppName->setValue(priv->appName);
+  lblAppName->setValue("About " + priv->appName);
   auto lblAppVersion = YUI::widgetFactory()->createLabel(toprightvbox, "");
   lblAppVersion->setValue(priv->appVersion);
   auto rt = YUI::widgetFactory()->createRichText(toprightvbox,"");
   rt->setValue(priv->appDescription);
   
   auto bottomhbox = YUI::widgetFactory()->createHBox(vbox);
-  auto infoButton = YUI::widgetFactory()->createPushButton(bottomhbox, "Info");
-  auto creditsButton = YUI::widgetFactory()->createPushButton(bottomhbox, "Credits");
+  if(priv->appInformation.compare("")!=0)
+  {
+    infoButton = YUI::widgetFactory()->createPushButton(bottomhbox, "Info");
+  }
+  
+  if(priv->appCredits.compare("")!=0)
+  {
+    creditsButton = YUI::widgetFactory()->createPushButton(bottomhbox, "Credits");
+  }
   auto cancelButton = YUI::widgetFactory()->createPushButton(bottomhbox, "Close");
   while(true)
   {
@@ -102,15 +257,27 @@ void YMGAAboutDialog::start()
         break; // leave event loop
       else if( event->widget() == infoButton )
       {
-	
+	this->showInformation();
       }
       else if( event->widget() == creditsButton )
       {
-	
+	this->showCredits();
       }
     }
   }
   priv->mainDialog->destroy();
+}
+
+void YMGAAboutDialog::start(int type)
+{
+  if(type == 0)
+  {
+    this->Classic();
+  }
+  else
+  {
+   this->Tabbed(); 
+  }
 }
 
 YDialog* YMGAAboutDialog::getYDialog()
@@ -132,7 +299,7 @@ void YMGAAboutDialog::setAppLicense(const std::string& license)
 }
 void YMGAAboutDialog::setAppAuthor(const std::string& author)
 {
-  priv->appAuthor = author;
+  priv->appAuthors = author;
 }
 
 const std::string& YMGAAboutDialog::getAppName() const
@@ -149,7 +316,7 @@ const std::string& YMGAAboutDialog::getAppLicense() const
 }
 const std::string& YMGAAboutDialog::getAppAuthor() const
 {
-  return priv->appAuthor;
+  return priv->appAuthors;
 }
 
 const std::string& YMGAAboutDialog::getAppDescription() const

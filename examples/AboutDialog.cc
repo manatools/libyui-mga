381e390e1d2214b6136ff6821f6776dc8f4a7162
_pmat_ <matteo@mageia.it>
2014-03-26 00:16:08 +0100

    using logo

--- examples/AboutDialog.cc
+++ examples/AboutDialog.cc
@@ -47,7 +47,8 @@ int main( int argc, char **argv )
     const std::string desc = "This is a <b>demo</b> to show you how <em>beautiful</em> is the <u>builtin</u> AboutDialog<br />";
     const std::string authors = "<h3>Developers</h3><ul><li>Angelo Naselli</li><li>Matteo Pasotti</li></ul><h3>Translators</h3>";
     const std::string credits = "(C) 2013-2014 Angelo Naselli\n(C) 2013-2014 Matteo Pasotti";
-    const std::string icon = "/usr/lib/libDrakX/icons/IC-Dhost-48.png";
+    const std::string logo = "/usr/lib/libDrakX/icons/IC-Dhost-48.png";
+    const std::string icon = "";
     const std::string info = "Displaying <strong>useful</strong> information";
 
     YCommandLine cmdline;
@@ -70,6 +71,7 @@ int main( int argc, char **argv )
                                       "GPLv2",
                                       authors,
                                       desc,
+                                      logo,
                                       icon,
                                       credits,
                                       info

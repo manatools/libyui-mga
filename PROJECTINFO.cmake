dd8a06a9d41662b2ab042412a534fb0b761cc509
Angelo Naselli <anaselli@linux.it>
2016-01-18 22:20:03 +0100

    1.0.7

--- PROJECTINFO.cmake
+++ PROJECTINFO.cmake
@@ -11,7 +11,7 @@ SET( INTERNAL_DEPS	Libyui )	# include libyui or it's plugins (if needed), too!
 #SET( LIB_LINKER		QtCore QtGui X11 fontconfig dl pthread )	# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!
 #SET( EXTRA_INCLUDES	)		# set include-dir which are not picked by CMake automagically here.
 SET( PROGSUBDIR		"" )		# define your programm's name if the plugin is made for it, only. e.g. "YaST2" for {gtk,ncurses,qt}-pkg.
-SET( URL                "http://Todo-mga-repo/" )
+SET( URL                "https://github.com/xquiet/libyui-mga" )
 SET( SUMMARY            "Libyui - Mageia extension plugin" )
 SET( DESCRIPTION        "This package contains the Mageia extension user interface\ncomponent for libYUI.\n" )
 
